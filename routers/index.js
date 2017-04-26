26977dedca469e8458dadc972350e752b20faebd
markmarkyesyes <markhahn545@gmail.com>
2017-04-26 16:33:36 -0500

    completed points updating

--- routers/index.js
+++ routers/index.js
@@ -12,6 +12,7 @@ const passport = require("passport");
 ////
 router.get("/", (req, res) => {
   if (req.user) {
+    console.log(req.user);
     res.render("home", {user: req.user});
   } else {
     res.redirect("/login");
@@ -37,67 +38,67 @@ router.post(
 );
 
 let pointsModel = distance => {
-  console.log("Distance: ", distance);
   let ponzPoints = [0, 40, 20, 10, 5, 2];
   if (distance > 5) {
-    console.log("Inside points function, returning: ", 1)
     return 1;
   } else {
-    console.log("Inside points function, returning: ", ponzPoints[distance])
     return ponzPoints[distance];
   }
 };
 
 let updateAncestorPoints = (parentId, createdUserLevel) => {
-  return new Promise(function(resolve, reject) {
-    let points;
-    User.findById(parentId)
-      .then(parent => {
-        points = pointsModel(createdUserLevel - parent.level);
-        parent.points += parseInt(points);
-        return parent.save();
-      })
-      .then(result => {
-        if (!result.parentId === null) {
-          updateAncestorPoints(result.parentId, createdUserLevel);
-        } else {
-          resolve();
-        }
-      })
-      // .catch(err => {
-      //   // console.log(err);
-      // });
-  });
+  let points;
+  console.log("ancestor points parentId", parentId);
+  return User.findById(parentId)
+    .then(parent => {
+      points = pointsModel(createdUserLevel - parent.level);
+      parent.points += parseInt(points);
+      return parent.save();
+    })
+    .then(result => {
+      if (result.parentId) {
+        return updateAncestorPoints(result.parentId, createdUserLevel);
+      } else {
+        console.log("exiting updateAncestorPoints");
+      }
+    });
 };
 
 router.post("/register/:id", (req, res, next) => {
   const {username, password} = req.body;
   const children = [];
-  const parentId = req.params.id;;
+  const parentId = req.params.id;
   const points = 0;
   const level = 1;
-  const user = new User({username, password, children, parentId, points, level});
+  const user = new User({
+    username,
+    password,
+    children,
+    parentId,
+    points,
+    level
+  });
   user.save((err, user) => {
-    console.log("User after first being created: ", user);
     User.findByIdAndUpdate(parentId, {
-      $push: {"children": user._id}
+      $push: {children: user._id}
     })
       .then(parentUser => {
         let newUserLevel = parentUser.level + 1;
         user.level = newUserLevel;
         return user.save();
-        // return User.findByIdAndUpdate(user._id, {$set: {"level": newUserLevel}});
       })
       .then(updatedUser => {
-        console.log("User created and updated: ", updatedUser)
-        return updateAncestorPoints(updatedUser.parentId, updatedUser.level);
-      })
-      .then(() => {
-        req.login(user, function(err) {
-          if (err) {
-            return next(err);
-          }
-          return res.redirect("/");
+        updateAncestorPoints(
+          updatedUser.parentId,
+          updatedUser.level
+        ).then(() => {
+          console.log("entered res scope");
+          req.login(updatedUser, function(err) {
+            if (err) {
+              return next(err);
+            }
+            return res.redirect("/");
+          });
         });
       });
   });
