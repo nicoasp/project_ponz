eea09b2b24cfcb3938cb5c3b1b8780551b4a00c3
nicoasp <nicolas.amaya@gmail.com>
2017-04-26 18:03:40 +0200

    finish sessions setup

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ app.use(flash());
 //Mongoose
 ////
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/passport-app");
-// const models = require("./models");
-// const User = models.User;
+mongoose.connect("mongodb://localhost/ponzio");
+const models = require("./models");
+const User = models.User;
+
 
 ////
 //Passport
@@ -40,18 +41,35 @@ const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
-// ////
-// //Passport Sessions
-// ////
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
-//
-// passport.deserializeUser(function(id, done) {
-//   User.findById(id, function(err, user) {
-//     done(err, user);
-//   });
-// });
+// Local Strategy Set Up
+
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username }, function(err, user) {
+      console.log(user);
+      if (err) return done(err);
+      if (!user || !user.validPassword(password))
+        return done(null, false, { message: "Invalid username/password" });
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
+
 
 ////
 //Handlebars
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const {User} = require("../models")
+
 ////
 //Passport & Strategies
 ////
@@ -9,20 +11,46 @@ const passport = require("passport");
 //Login Routes
 ////
 router.get("/", (req, res) => {
-  // console.log(req.user);
-  // if (req.user) {
-  res.render("home");
-  // } else {
-  //   res.redirect("/login");
-  // }
+  if (req.user) {
+    res.render("home", { user: req.user });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/logout", (req, res) => {
-  res.redirect("/login");
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  user.save((err, user) => {
+    req.login(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  });
+});
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
